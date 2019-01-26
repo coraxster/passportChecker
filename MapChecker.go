32e665f7af8912353946d78d51b9e09ebcae1d32
dmitry kuzmin <dmitry.kuzmin@exness.com>
2019-01-26 19:35:18 +0200

    simple map

--- MapChecker.go
+++ MapChecker.go
@@ -1,35 +1,22 @@
 package passportChecker
 
 import (
-	"errors"
 	"fmt"
-	"log"
 )
 
 type MapChecker struct {
-	m1 map[[10]Value]struct{}
-	m2 map[[10]Value]struct{}
+	m1 map[string]struct{}
+	m2 map[string]struct{}
 }
 
+//7.2gb, low cpu, fast resolves, handmade, easy to persist on disk, GC
 func MakeMapChecker() *MapChecker {
-	return &MapChecker{make(map[[10]Value]struct{}), make(map[[10]Value]struct{})}
+	return &MapChecker{make(map[string]struct{}), make(map[string]struct{})}
 }
 
 func (c *MapChecker) Add(values []interface{}) error {
 	for _, val := range values {
-		vs, err := stringToChain(fmt.Sprint(val))
-		if err != nil {
-			log.Print(err.Error())
-			continue
-		}
-		if len(vs) > 10 {
-			log.Print("too big string:" + fmt.Sprint(val))
-			continue
-		}
-		var key [10]Value
-		for i, v := range vs {
-			key[i] = v
-		}
+		key := fmt.Sprint(val)
 		b := *c.bucket(key)
 		b[key] = struct{}{}
 	}
@@ -42,17 +29,7 @@ func (c *MapChecker) Check(values []interface{}) ([]bool, error) {
 	}
 	result := make([]bool, 0, len(values))
 	for _, val := range values {
-		vs, err := stringToChain(fmt.Sprint(val))
-		if err != nil {
-			return nil, err
-		}
-		if len(vs) > 12 {
-			return nil, errors.New("too big string:" + fmt.Sprint(val))
-		}
-		var key [10]Value
-		for i, v := range vs {
-			key[i] = v
-		}
+		key := fmt.Sprint(val)
 		b := *c.bucket(key)
 		_, ok := b[key]
 		result = append(result, ok)
@@ -60,8 +37,8 @@ func (c *MapChecker) Check(values []interface{}) ([]bool, error) {
 	return result, nil
 }
 
-func (c *MapChecker) bucket(k [10]Value) *map[[10]Value]struct{} {
-	if k[4] > Value(25) {
+func (c *MapChecker) bucket(k string) *map[string]struct{} {
+	if []rune(k)[3] > '5' {
 		return &c.m1
 	}
 	return &c.m2
