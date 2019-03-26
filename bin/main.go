f583f498580cc279f111b10519e1fc548819021f
dmitry kuzmin <corax@list.ru>
2019-03-26 23:31:43 +0300

    update

--- bin/main.go
+++ bin/main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"bufio"
+	"compress/bzip2"
 	"context"
 	"flag"
 	"github.com/coraxster/passportChecker"
 	"github.com/dgraph-io/badger"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mileusna/crontab"
+	"github.com/pkg/errors"
 	"github.com/seiflotfy/cuckoofilter"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
+	"sync"
 )
 
 const CuckooCapacity = 200000000
-const CuckooFilename = "cuckoo.data"
 
-var parseFile = flag.String("parseFile", "", "parse file on start")
+var url = flag.String("url", "https://guvm.mvd.ru/upload/expired-passports/list_of_expired_passports.csv.bz2", "passport numbers bz2 url")
 var port = flag.String("port", "80", "serve port")
+var updateCron = flag.String("updateCron", "0 2 * * *", "when update")
+var updateOnStart = flag.Bool("updateOnStart", false, "update on start")
+
+var parseMutex sync.Mutex
 
 func main() {
 	flag.Parse()
@@ -37,94 +41,137 @@ func main() {
 
 	ctx := makeContext()
 
-	opts := badger.DefaultOptions
-	opts.Dir = "./badger"
-	opts.ValueDir = "./badger"
-	opts.SyncWrites = false
-	bdb, err := badger.Open(opts)
-	checkError(err)
+	bdb := getBadger()
 	defer bdb.Close()
 	badCh := passportChecker.MakeBadgerChecker(bdb)
 
-	f, err := getCuckoo(CuckooCapacity)
-	log.Println("Cuckoo size: ", f.Count())
+	filter, err := getCuckoo(bdb)
 	checkError(err)
-	CuCh, err := passportChecker.MakeCuckooChecker(f)
+	log.Println("INFO: Cuckoo size: ", filter.Count())
+	CuCh, err := passportChecker.MakeCuckooChecker(filter)
 	checkError(err)
 
 	ch := passportChecker.MakeMultiChecker(CuCh, badCh)
 
-	parseDone := make(chan struct{})
-	if len(*parseFile) > 0 {
-		go func() {
-			log.Print("parsing file: " + *parseFile)
-			err = AddCSVFile(ctx, ch, *parseFile)
-			if err != nil {
-				log.Print(err)
-				return
-			}
-			runtime.GC()
-			saveCuckoo(f)
-			close(parseDone)
-		}()
-	} else {
-		close(parseDone)
+	updateFunc := func() {
+		parseMutex.Lock()
+		defer parseMutex.Unlock()
+		changed, err := updateChecker(ctx, ch, bdb, *url)
+		if err != nil {
+			log.Print("ERROR: while updating. ", err.Error())
+			return
+		}
+		if changed {
+			saveCuckoo(bdb, filter)
+		}
 	}
 
+	if *updateOnStart {
+		go func() {
+			updateFunc()
+		}()
+	}
+	log.Println("INFO: Crontab config: ", *updateCron)
+	ctab := crontab.New()
+	err = ctab.AddJob(*updateCron, updateFunc)
+	checkError(err)
+
+	err = startServer(ctx, ch)
+	checkError(err)
+
+	parseMutex.Lock()
+}
+
+func startServer(ctx context.Context, ch *passportChecker.MultiChecker) error {
 	r := chi.NewRouter()
 	h := passportChecker.MakeHandler(ch)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Get("/check/{value}", h.Check)
-
 	srv := http.Server{Addr: ":" + *port, Handler: r}
-	log.Print("starting serving on :" + *port)
+	log.Print("INFO: starting serving on :" + *port)
 	go func() {
 		err := srv.ListenAndServe()
 		if err != http.ErrServerClosed {
 			checkError(err)
 		}
 	}()
-
 	<-ctx.Done()
-	err = srv.Shutdown(context.Background())
-	checkError(err)
-
-	<-parseDone
+	return srv.Shutdown(context.Background())
 }
 
-func AddCSVFile(ctx context.Context, ch passportChecker.ExistChecker, path string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	reader := bufio.NewReader(file)
-	_, _, err = reader.ReadLine() // skip header line
-	if err != nil {
-		return err
-	}
+func getBadger() *badger.DB {
+	opts := badger.DefaultOptions
+	opts.Dir = "./badger"
+	opts.ValueDir = "./badger"
+	opts.SyncWrites = false
+	opts.ValueLogMaxEntries = 100000000
+	bdb, err := badger.Open(opts)
+	checkError(err)
+	return bdb
+}
 
+func updateChecker(ctx context.Context, ch passportChecker.ExistChecker, bdb *badger.DB, url string) (bool, error) {
+	log.Print("INFO: parsing url: " + url)
+	localVersion, err := getLocalVersion(bdb)
+	if err != nil {
+		return false, err
+	}
+	log.Println("INFO: Local bz2 version: ", localVersion)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return false, err
+	}
+	req.Header.Add("If-None-Match", localVersion)
+	resp, err := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return false, err
+	}
+	if resp.StatusCode == 304 {
+		log.Println("INFO: Remote version has not changed")
+		return false, nil
+	}
+	remoteVersion := resp.Header.Get("ETag")
+	if remoteVersion == "" {
+		return false, errors.New("Server returns response without ETag header")
+	}
+	log.Println("INFO: Remote bz2 version: ", remoteVersion)
+	reader := bufio.NewReader(bzip2.NewReader(resp.Body))
+	err = fillChecker(ctx, ch, reader)
+	if err != nil {
+		return false, err
+	}
+	return true, setLocalVersion(bdb, remoteVersion)
+}
+
+func fillChecker(ctx context.Context, ch passportChecker.ExistChecker, reader *bufio.Reader) error {
+	log.Println("INFO: Start filling ")
 	chunkCh := make(chan []string)
+	defer close(chunkCh)
 	for i := 0; i < runtime.NumCPU(); i++ {
 		go func() {
 			for chunk := range chunkCh {
-				err = ch.Add(chunk)
+				err := ch.Add(chunk)
 				if err != nil {
-					close(chunkCh)
-					log.Printf("Add error: %v", err)
+					log.Printf("ERROR: Add error: %v", err)
 				}
 			}
 		}()
 	}
 	chunk := make([]string, 0, 1000)
 	var readCount uint
+	var err error
+forLoop:
 	for {
 		select {
 		case <-ctx.Done():
-			break
+			err = ctx.Err()
+			break forLoop
 		default:
 		}
-		line, _, err := reader.ReadLine()
+		var line []byte
+		line, _, err = reader.ReadLine()
 		if err == io.EOF {
 			err = nil
 			break
@@ -139,27 +186,73 @@ func AddCSVFile(ctx context.Context, ch passportChecker.ExistChecker, path strin
 		}
 		readCount++
 		if readCount%1000000 == 0.0 {
-			log.Printf("read: %v", readCount)
+			log.Printf("INFO: Read: %v", readCount)
 		}
 	}
-	close(chunkCh)
+	log.Printf("INFO: Read done: %v", readCount)
 	return err
 }
 
-func getCuckoo(cap uint) (*cuckoo.Filter, error) {
-	b, _ := ioutil.ReadFile("./" + CuckooFilename)
+func getLocalVersion(bdb *badger.DB) (string, error) {
+	var localVersion string
+	err := bdb.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("bzVersion"))
+		if err == badger.ErrKeyNotFound {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		err = item.Value(func(v []byte) error {
+			localVersion = string(v)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return localVersion, err
+}
+
+func setLocalVersion(bdb *badger.DB, localVersion string) error {
+	return bdb.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte("bzVersion"), []byte(localVersion))
+	})
+}
+
+func getCuckoo(db *badger.DB) (*cuckoo.Filter, error) {
+	log.Print("INFO: reading Cuckoo..")
+	b := make([]byte, 0)
+	err := db.View(func(txn *badger.Txn) error {
+		i, err := txn.Get([]byte("Cuckoo"))
+		if err == badger.ErrKeyNotFound {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		b, err = i.ValueCopy(b)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
 	if len(b) == 0 {
-		return cuckoo.NewFilter(cap), nil
+		return cuckoo.NewFilter(CuckooCapacity), nil
 	}
 	return cuckoo.Decode(b)
 }
 
-func saveCuckoo(f *cuckoo.Filter) {
-	log.Print("saving Cuckoo..")
-	err := ioutil.WriteFile("./"+CuckooFilename, f.Encode(), 0644)
+func saveCuckoo(db *badger.DB, f *cuckoo.Filter) {
+	log.Print("INFO: saving Cuckoo..")
+	err := db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte("Cuckoo"), f.Encode())
+	})
 	if err != nil {
-		log.Printf("rerror while saving Cuckoo: %v", err.Error())
+		log.Printf("ERROR: saving Cuckoo failed: %v", err.Error())
 	}
+	log.Print("INFO: cuckoo saved")
 }
 
 func checkError(err error) {
